7fec9a8e6e5fe7ef743caba7a751281038f67b56
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-08-26 09:33:19 -0600

    updated project

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,24 +1,27 @@
 $(document).ready(function() {
+  //ALL CODE GOES IN HERE
 
-
-$('#newTaskForm').hide();                   // Allows newTaskForm to be hidden when the document loads
-
-  // Variables
-var listo = [];
-var Task = function(task) {
-  this.task = task;
-  this.id = 'new';
+var listo = [];  //array to hold all tasks - global variable
+var Task = function(task) {  //Constructor function to add tasks
+  this.task = task;          //to the array
+  this.id ='new';
 };
 
-                                                                                  ////// Functions
-// Function for
+/////// GET SAVED TASKS FROM LOCAL STORAGE \\\\\\\\\\
+$
+('#new').html(localStorage.getItem('task'));
+$('#inProgress').html(localStorage.getItem('task'));
+$('#archived').html(localStorage.getItem('task'));
+
+//
+
 var advanceTask = function(task) {
   var modified = task.innerText.trim();
   for (var i = 0; i < listo.length; i++) {
     if (listo[i].task === modified) {
       if (listo[i].id === 'new') {
         listo[i].id = 'inProgress';
-      } else if (listo[i].id === 'inProgress') {
+      } else if (listo[i].id === 'currentList') {
         listo[i].id = 'archived';
       } else {
         listo.splice(i, 1);
@@ -29,54 +32,15 @@ var advanceTask = function(task) {
   task.remove();
 };
 
-// Function to change the status of an item from 'new' to 'inProgress'.
-$(document).on('click', '#item', function(e) {
-	e.preventDefault();
-  var task = this;
-  advanceTask(task);
-  this.id = 'inProgress';
-});
+////////////\\\\\\\\\\\\\
+// ADD NEW TASK \\--------------------------
 
+var addTask = function(task) {
+	if(task) {
+		task = new Task(task);
+		listo.push(task);
 
-// Function to move the actual list item.
-// We do that by pulling all of the html around the item itself.
-$(document).on('click', '#item', function(e) {
-	e.preventDefault();
-  var task = this;
-  advanceTask(task);
-  this.id = 'inProgress';
-  $('#currentList').append(this.outerHTML);
-});
-
-// Function to move the items from 'inProgress' to 'archived'
-$(document).on('click', '#inProgress', function(e) {
-  e.preventDefault();
-  var task = this;
-  task.id = "archived";
-  var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
-  advanceTask(task);
-  $('#archivedList').append(changeIcon);
-});
-
-// Function to create a way to delete the items on the list
-  // All we have to do is pass a task into the advanceTask function without a new id.
-  // You can study the advanceTask function we built to understand how it works.
-$(document).on('click', '#archived', function(e){
-  e.preventDefault();
-  var task = this;
-  advanceTask(task);
-});
-
-
-
-///////////////////////////////                               EventListeners
-
-var addTask = function(task) {                          // Task constructor with several functions
-  if(task) {
-    task = new Task(task);
-    listo.push(task);
-
-    $('#newItemInput').val('');
+		$('#newItemInput').val('');
 		  $('#newList').append(
                         '<a href="#finish" class="" id="item">' +
                         '<li class="list-group-item">' +
@@ -87,26 +51,69 @@ var addTask = function(task) {                          // Task constructor with
                         '</li>' +
                         '</a>'
                     );
-  }
+
+	}
   $('#newTaskForm').slideToggle('fast', 'linear');
-  };
+  localStorage.setItem(listo, task.task);
+};
 
-  // Call a jQuery event that calls the addTask function when we click the saveNewItem button.
-  $('#saveNewItem').on('click', function (e) { // Call the "addTask" function
-      e.preventDefault();
-      var task = $('#newItemInput').val().trim();
-      addTask(task);
-  });
-
-  // Opens the form to create new task:
-  $('#add-todo').on('click', function() {
-    $('#newTaskForm').fadeToggle('fast', 'linear');
-  });
-
-  // Closes the form to create new task:
-  $('#cancel').on('click', function(){
+$('#saveNewItem').on('click', function (e) {
     e.preventDefault();
-    $('#newTaskForm').fadeToggle('fast', 'linear');
+    var task = $('#newItemInput').val().trim();
+    addTask(task);
+});
+
+
+//Opens form---------------
+
+  $('#add-todo').on('click', function () {
+      $('#newTaskForm').fadeToggle('fast', 'linear');
   });
 
+
+//closes form---------------
+
+  $('#cancel').on('click', function (e) {
+      e.preventDefault();
+      $('#newTaskForm').fadeToggle('fast', 'linear');
+  });
+
+
+//moves task from new to in progress---------------
+
+  $(document).on('click', '#item', function(e) {
+  	e.preventDefault();
+    var task = this;
+    advanceTask(task);
+    this.id = 'inProgress';
+    $('#currentList').append(this.outerHTML);
+  });
+
+
+//moves task from in progress to archived---------------
+
+  $(document).on('click', '#inProgress', function (e) {
+    e.preventDefault();
+    var task = this;
+    task.id = "archived";
+    var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+    advanceTask(task);
+    $('#archivedList').append(changeIcon);
+  });
+
+
+//moves deletes archived item---------------
+
+$(document).on('click', '#archived', function (e) {
+  e.preventDefault();
+  var task = this;
+  advanceTask(task);
+});
+
+
+
+
+
+
+  //ALL CODE GOES IN HERE
 });
